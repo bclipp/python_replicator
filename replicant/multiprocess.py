a4c3151255f9b2e3b252eb0017a39b77912d73b7
blipp <bclipp770@gmail.com>
2020-07-13 20:01:01 -0400

    added one test and cleaned things up

--- replicant/multiprocess.py
+++ replicant/multiprocess.py
@@ -1,14 +1,27 @@
+"""
+This module is used for abstracted uses of the multiprocess library.
+"""
+
 import multiprocessing as mp
 from typing import Callable, Optional
 
 
-def multiprocess_me(size: Optional[int],
+def multiprocess_me(size: int,
                     func: Callable,
                     data: list,
                     output: bool = True) -> Optional[list]:
+    """
+    multiprocess_me is used to multiprocess across a list of dicts.
+
+    :param size:  The amount of parallelism.
+    :param func: the function to use on each dict
+    :param data: a list of dicts.
+    :param output: Should multiprocess_me return a list of Dict?
+    :return:
+    """
     pool = mp.Pool(size)
     updated_data: list = pool.map(func, data)
     pool.close()
+    pool.join()
     if output:
-        pool.join()
         return updated_data
---
+++ tests/test_multiprocess.py
@@ -0,0 +1,42 @@
+"""
+tests for the multiprocess
+"""
+import unittest
+
+import pandas as pd
+
+import replicant.multiprocess as mult
+
+
+def test_multiprocess_me_side_effect():
+    """
+    Used to test that mlflow is working
+    :param data_frame:
+    :param wanted:
+    :return:
+    """
+    data_frame: pd.DataFrame = pd.DataFrame(
+        columns=["This",
+                 "is",
+                 "a",
+                 "test",
+                 "to",
+                 "check",
+                 "a_function"],
+        data=[[5, 2, "b", 3, 4, 5, 3],
+              [7, 2, "b", 3, 4, 5, 3],
+              [8, 2, "b", 3, 4, 5, 3],
+              [3, 2, "b", 3, 4, 5, 3],
+              [2, 2, "b", 3, 4, 5, 3],
+              [8, 2, "b", 3, 4, 5, 3],
+              [2, 2, "b", 3, 4, 5, 3],
+              [8, 2, "b", 3, 4, 5, 3],
+              [19, 2, "b", 3, 4, 5, 3],
+              [53, 2, "b", 3, 4, 5, 3],
+              [11, 2, "b", 3, 4, 5, 3],
+              [16, 2, "b", 3, 4, 5, 3]])
+    data: list = list(data_frame.T.to_dict().values())
+    mult.multiprocess_me(1, print, data, False)
+
+
+
